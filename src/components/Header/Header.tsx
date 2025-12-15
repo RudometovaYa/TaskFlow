998ed1e2d8a8e8d95042c6e92f457fe7e8500c0a
Yana Rudometova <rudometova.ya@gmail.com>
2025-12-15 23:31:37 +0200

    add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,12 @@ export default function Header() {
   return (
     <header className={css.header}>
       <Link to="/" aria-label="Home" className={css.logo}>
-        TaskFlow
+        <img
+          src="/free-icon-check-lists-16989981.png" // <- файл icon.png має бути у папці public
+          alt="Logo"
+          className={css.logoImage}
+        />
+        TF
       </Link>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
